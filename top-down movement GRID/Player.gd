9356fbcd02f489d8f84dd427947ccda0f499b951
awindahl <awindahl@kth.se>
2017-06-17 14:44:30 +0200

    added basic attack

--- top-down movement GRID/Player.gd
+++ top-down movement GRID/Player.gd
@@ -14,6 +14,9 @@ var is_moving = false
 var type
 var grid
 
+onready var attack = get_node("Attack");
+var attack_wait = 0
+var attack_val = 0
 
 func _ready():
 	grid = get_parent()
@@ -25,13 +28,13 @@ func _fixed_process(delta):
 	direction = Vector2()
 	speed = 0
 
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_up") && attack_val == 0:
 		direction.y = -1
-	elif Input.is_action_pressed("move_down"):
+	elif Input.is_action_pressed("move_down") && attack_val == 0:
 		direction.y = 1
-	elif Input.is_action_pressed("move_left"):
+	elif Input.is_action_pressed("move_left") && attack_val == 0:
 		direction.x = -1
-	elif Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right") && attack_val == 0:
 		direction.x = 1
 
 	if not is_moving and direction != Vector2():
@@ -50,5 +53,19 @@ func _fixed_process(delta):
 		if move_distance > distance_to_target:
 			velocity = target_direction * distance_to_target
 			is_moving = false
-
+		
 		move(velocity)
+	
+	if Input.is_action_pressed("ui_accept") && attack_val == 0 && attack_wait == 0:
+		attack_wait = 20
+		attack.show()
+		attack_val = 30
+		attack.set_pos(target_direction * 60);
+	
+	if attack_val < 101 && attack_val > 0:
+		attack_val-=1
+	
+	if attack_val == 0 && attack_wait > 0:
+		attack.hide()
+		attack_wait -= 1
+	
