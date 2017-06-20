558082702583547283ad65cfd302bd5d17fce445
awindahl <awindahl@kth.se>
2017-06-20 16:48:05 +0200

    new Grid movement

--- top-down movement GRID/Player.gd
+++ top-down movement GRID/Player.gd
@@ -1,54 +1,39 @@
 extends KinematicBody2D
 
 var direction = Vector2()
+var start_pos = Vector2()
 
-const MAX_SPEED = 400
-
-var speed = 0
-var velocity = Vector2()
-
-var target_pos = Vector2()
-var target_direction = Vector2()
 var is_moving = false
 
-var type
-var grid
+const GRID = 64
+const SPEED = 4
+
+onready var look = get_node("Looking")
 
 func _ready():
-	grid = get_parent()
-	type = grid.PLAYER
 	set_fixed_process(true)
 
 func _fixed_process(delta):
 	
-	direction = Vector2()
-	speed = 0
-
-	if Input.is_action_pressed("move_up"):
-		direction.y = -1
-	elif Input.is_action_pressed("move_down"):
-		direction.y = 1
-	elif Input.is_action_pressed("move_left"):
-		direction.x = -1
-	elif Input.is_action_pressed("move_right"):
-		direction.x = 1
-
-	if not is_moving and direction != Vector2():
-		target_direction = direction.normalized()
-		if grid.is_cell_vacant(get_pos(), direction):
-			target_pos = grid.update_child_pos(get_pos(), direction, type)
-			is_moving = true
-	elif is_moving:
-		speed = MAX_SPEED
-		velocity = speed * target_direction * delta
-
-		var pos = get_pos()
-		var distance_to_target = pos.distance_to(target_pos)
-		var move_distance = velocity.length()
-
-		if move_distance > distance_to_target:
-			velocity = target_direction * distance_to_target
+	if !is_moving && !look.is_colliding():
+		if Input.is_action_pressed("move_up"):
+			is_moving=true
+			direction = Vector2(0,-1)
+			start_pos=get_pos()
+			look.set_rot(deg2rad(-180))
+		elif Input.is_action_pressed("move_down"):
+			is_moving=true
+			direction = Vector2(0,1)
+			start_pos=get_pos()
+		elif  Input.is_action_pressed("move_left"):
+			is_moving=true
+			direction = Vector2(-1,0)
+			start_pos=get_pos()
+		elif  Input.is_action_pressed("move_right"):
+			is_moving=true
+			direction = Vector2(1,0)
+			start_pos=get_pos()
+	else:
+		move_to(get_pos() + direction * SPEED)
+		if get_pos() == start_pos +Vector2(GRID*direction.x, GRID*direction.y):
 			is_moving = false
-		
-		move(velocity)
-	
