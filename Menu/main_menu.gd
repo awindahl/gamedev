977705d2c0f15981609fd4658b525b2ef925ec4f
awindahl <awindahl@kth.se>
2017-06-20 22:34:40 +0200

    renamed grid movement folder, also turning works

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -16,4 +16,4 @@ func _on_Quit_pressed():
 	get_tree().quit()
 
 func _on_NewGame_pressed():
-	get_tree().change_scene("res://top-down movement GRID/Game.tscn")
+	get_tree().change_scene("res://grid-movement/Game.tscn")
--- grid-movement/Player.gd
+++ grid-movement/Player.gd
@@ -18,9 +18,17 @@ func _ready():
 
 func _fixed_process(delta):
 	
-	if look.is_colliding():
-		print("imgay")
+	#-------Handles looking control-----#
+	if Input.is_action_pressed("move_up"):
+		look.set_rot(deg2rad(180))
+	elif Input.is_action_pressed("move_down"):
+		look.set_rot(deg2rad(0))
+	elif Input.is_action_pressed("move_left"):
+		look.set_rot(deg2rad(240))
+	elif Input.is_action_pressed("move_right"):
+		look.set_rot(deg2rad(90))
 	
+	#-------Handles grid movement-------#
 	if !is_moving:
 		
 		var resultUp = world.intersect_point(get_pos() + Vector2(0, -GRID))
@@ -32,22 +40,18 @@ func _fixed_process(delta):
 			is_moving=true
 			direction = Vector2(0,-1)
 			start_pos=get_pos()
-			look.set_rot(deg2rad(180))
 		elif Input.is_action_pressed("move_down") && resultDown.empty():
 			is_moving=true
 			direction = Vector2(0,1)
 			start_pos=get_pos()
-			look.set_rot(deg2rad(0))
 		elif  Input.is_action_pressed("move_left") && resultLeft.empty():
 			is_moving=true
 			direction = Vector2(-1,0)
 			start_pos=get_pos()
-			look.set_rot(deg2rad(240))
 		elif  Input.is_action_pressed("move_right") &&resultRight.empty():
 			is_moving=true
 			direction = Vector2(1,0)
 			start_pos=get_pos()
-			look.set_rot(deg2rad(90))
 	else:
 		move_to(get_pos() + direction * SPEED)
 		if get_pos() == start_pos +Vector2(GRID*direction.x, GRID*direction.y):
