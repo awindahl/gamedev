02da95077738a977c3f2f342cab14cb37c1f192a
awindahl <awindahl@kth.se>
2017-07-14 20:56:28 +0200

    fixed node movement AI

---
+++ grid-movement/Enemy.gd
@@ -0,0 +1,35 @@
+
+extends RigidBody2D
+
+# travel speed in pixel/s
+export var speed = 200
+onready var end = get_node("Position2D").get_global_pos()
+
+# at which distance to stop moving
+# NOTE: setting this value too low might result in jerky movement near destination
+const eps = 1.5
+
+# points in the path
+var points = []
+
+func _ready():
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	# refresh the points in the path
+	points = get_node("../Navigation2D").get_simple_path(get_global_pos(), end, false)
+	# if the path has more than one point
+	if points.size() > 1:
+		var distance = points[1] - get_global_pos()
+		var direction = distance.normalized() # directio-n of movement
+		if distance.length() > eps or points.size() > 2:
+			set_linear_velocity(direction*speed)
+		else:
+			set_linear_velocity(Vector2(0, 0)) # close enough - stop moving
+		update() # we update the node so it has to draw it self again
+
+func _draw():
+	# if there are points to draw
+	if points.size() > 1:
+		for p in points:
+			draw_circle(p - get_global_pos(), 8, Color(1, 0, 0)) # we draw a circle (convert to global position first)
--- grid-movement/Game.gd
+++ grid-movement/Game.gd
@@ -1,14 +1,7 @@
 extends Node
 
-onready var e = get_node("Enemy1")
-onready var end_pos = get_node("EndPos")
-onready var nav = get_node("nav")
-onready var start_pos = get_node("StartPos").get_pos()
-
-
 func _ready():
 	set_process_input(true)
-	set_process(true)
 	set_pause_mode(PAUSE_MODE_PROCESS)
 
 func _input(event):
@@ -17,14 +10,3 @@ func _input(event):
 			get_tree().set_pause(false)
 		else:
 			get_tree().set_pause(true)
-
-
-func _process(delta):
-	
-	if !e.hasReached:
-		e.goal = end_pos.get_pos()
-		e.nav = nav
-	else:
-		var i = rand_range(1.0,20.0)
-		end_pos.set_pos(Vector2(i,i))
-		e.hasReached=false
