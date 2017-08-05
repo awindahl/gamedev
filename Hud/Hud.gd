f3701f2fac7db851ee48980378c1fac3f47f7183
sumaris <alfred.radu@gmail.com>
2017-08-06 01:13:36 +0200

    Added the different hud screens and swapping between them

--- Hud/Hud.gd
+++ Hud/Hud.gd
@@ -10,7 +10,7 @@ var color
 
 func _ready():
 	
-	get_node("Panel/MPLabel").set_text(var2str(currentMP) + "/" + var2str(myMP))
+	get_node("HUD/MPLabel").set_text(var2str(currentMP) + "/" + var2str(myMP))
 	if main.myClass == "Fighter":
 		color = "FFA500"
 	elif main.myClass == "Rogue":
@@ -19,25 +19,24 @@ func _ready():
 		color = "FFFF00"
 	elif main.myClass == "Mage":
 		color = "0000FF"
-	get_node("Panel/MPActual").set_frame_color(color)
+	get_node("HUD/MPActual").set_frame_color(color)
 	set_fixed_process(true)
 	
 func _fixed_process(delta):
 	
-	
 	if usingMP:
 		currentMP = currentMP - changeInMP*e;
-		diff = floor(get_node("Panel/MPOutline").get_size().x*(float(changeInMP)/float(myMP)))
+		diff = floor(get_node("HUD/MPOutline").get_size().x*(float(changeInMP)/float(myMP)))
 		if currentMP <= 0:
 			currentMP = 0
-			diff = get_node("Panel/MPActual").get_size().x
+			diff = get_node("HUD/MPActual").get_size().x
 			get_node("Button").set_disabled(true)
 		if currentMP > myMP:
 			currentMP = myMP
-			diff = get_node("Panel/MPOutline").get_size().x-get_node("Panel/MPActual").get_size().x
+			diff = get_node("HUD/MPOutline").get_size().x-get_node("HUD/MPActual").get_size().x
 			
-		get_node("Panel/MPActual").set_size(Vector2(get_node("Panel/MPActual").get_size().x-e*diff,get_node("Panel/MPActual").get_size().y))
-		get_node("Panel/MPLabel").set_text(var2str(currentMP) + "/" + var2str(myMP))
+		get_node("HUD/MPActual").set_size(Vector2(get_node("HUD/MPActual").get_size().x-e*diff,get_node("HUD/MPActual").get_size().y))
+		get_node("HUD/MPLabel").set_text(var2str(currentMP) + "/" + var2str(myMP))
 		usingMP = false;
 		changeInMP = 0;
 
--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,14 +1,62 @@
 extends Node
 
-onready var node = get_node("Navigation2D")
-
 func _ready():
 	set_process_input(true)
 	set_pause_mode(PAUSE_MODE_PROCESS)
 
 func _input(event):
-	if event.is_action_pressed("pause"):
+	if event.is_action_pressed("ui_character") and get_node("gui/Node2D/Character").is_hidden():
+		if !get_tree().is_paused():
+			_pause()
+		_close_all()
+		get_node("gui/Node2D/Character").set_hidden(false)
+	elif event.is_action_pressed("ui_character"):
+		_close_all()
+		_unpause()
+		
+	if event.is_action_pressed("ui_inventory") and get_node("gui/Node2D/Inventory").is_hidden():
+		if !get_tree().is_paused():
+			_pause()
+		_close_all()
+		get_node("gui/Node2D/Inventory").set_hidden(false)
+	elif event.is_action_pressed("ui_inventory"):
+		_close_all()
+		_unpause()
+		
+	if event.is_action_pressed("ui_map") and get_node("gui/Node2D/Map").is_hidden():
+		if !get_tree().is_paused():
+			_pause()
+		_close_all()
+		get_node("gui/Node2D/Map").set_hidden(false)
+	elif event.is_action_pressed("ui_map"):
+		_close_all()
+		_unpause()
+		
+	if event.is_action_pressed("pause") and get_node("gui/Node2D/Pause").is_hidden():
+		if !get_tree().is_paused():
+			_pause()
+		_close_all()
+		get_node("gui/Node2D/Pause").set_hidden(false)
+	elif event.is_action_pressed("pause"):
+		_close_all()
+		_unpause()
+		
+	if event.is_action_pressed("ui_exit"):
 		if get_tree().is_paused():
-			get_tree().set_pause(false)
+			_close_all()
+			_unpause()
 		else:
-			get_tree().set_pause(true)
+			_pause()
+			get_node("gui/Node2D/Pause").set_hidden(false)
+			
+func _unpause():
+	get_tree().set_pause(false)
+
+func _pause():
+	get_tree().set_pause(true)
+
+func _close_all():
+	get_node("gui/Node2D/Character").set_hidden(true)
+	get_node("gui/Node2D/Inventory").set_hidden(true)
+	get_node("gui/Node2D/Map").set_hidden(true)
+	get_node("gui/Node2D/Pause").set_hidden(true)
