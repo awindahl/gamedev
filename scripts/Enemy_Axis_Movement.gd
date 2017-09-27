3cf97165d16f7a64c79f37db26bd5e624774cd76
awindahl <awindahl@kth.se>
2017-09-27 03:29:44 +0200

    axis movement fully random, does not turn on hitting players

--- scripts/Enemy_Axis_Movement.gd
+++ scripts/Enemy_Axis_Movement.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+var s = seed(randi())
+
 export var speed = 4
 export var  is_x = false
 export var  is_y = false
@@ -11,40 +13,48 @@ onready var timer = get_node("WalkTimer")
 onready var container = get_node("ContainedTimer")
 
 var direction = Vector2(1,-1)
+var xlessthan = randi()%3
+var ylessthan = randi()%3
 var xmove = false
 var ymove = false
 
 
 func _ready():
+	randomize(s)
 	look_x.add_exception(self)
 	look_y.add_exception(self)
 	set_meta("Type", "Enemy")
+	container.set_wait_time(randi()%5+1)
+	timer.set_wait_time(randi()%4+1)
 	set_process(true)
 	
 func _process(delta):
+	
+	if ylessthan == xlessthan:
+		ylessthan = ylessthan + 1
 	
 	if self.is_colliding():
 		if get_collider().get_meta("Type") == "Player" && get_collider().get_meta("Damaged") == "False":
 			var play_hit = get_parent().get_node("Player")
 			play_hit._on_player_hit()
 
-	if look_x.is_colliding() && (look_x.get_collider().get_meta("Type") == "Map" || "Player" || look_x.get_collider().get_meta("Type") == "Enemy"):
+	if look_x.is_colliding() && (look_x.get_collider().get_meta("Type") == "Map" || look_x.get_collider().get_meta("Type") == "Enemy"):
 		look_x.rotate(deg2rad(180))
 		direction.x = direction.x * - 1
 		
-	if look_y.is_colliding() && (look_y.get_collider().get_meta("Type") == "Map" || "Player" || look_y.get_collider().get_meta("Type") == "Enemy"):
+	if look_y.is_colliding() && (look_y.get_collider().get_meta("Type") == "Map" || look_y.get_collider().get_meta("Type") == "Enemy"):
 		look_y.rotate(deg2rad(180))
 		direction.y = direction.y * - 1
 	
 	if is_x:
-		if floor(timer.get_time_left()) == 2:
+		if floor(timer.get_time_left()) == xlessthan:
 			move(Vector2(speed * direction.x, 0))
 			xmove = true
 		elif !ymove:
 			xmove = false
 			move(Vector2(0,0))
 	if is_y:
-		if floor(timer.get_time_left()) == 0:
+		if floor(timer.get_time_left()) == ylessthan:
 			move(Vector2(0,speed * direction.y))
 			ymove = true
 		elif !xmove:
@@ -53,7 +63,11 @@ func _process(delta):
 			
 	
 func _on_ContainedTimer_timeout():
-	print("IM TIMED OUT ASSHOLE")
+	xlessthan = randi()%3
+	ylessthan = randi()%3
+	if ylessthan == xlessthan:
+		ylessthan = ylessthan + 1
+	
 	look_x.rotate(deg2rad(180))
 	look_y.rotate(deg2rad(180))
 	direction = direction * -1
