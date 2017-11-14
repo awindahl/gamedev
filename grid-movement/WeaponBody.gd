51cc2d7a5842d5b85e92e0e59e8669cec776a5e7
sumaris <alfred.radu@gmail.com>
2017-11-14 21:12:35 +0100

    hello

---
+++ grid-movement/WeaponBody.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var selectedWeapon = "Unarmed"
+
+func _ready():
+	set_meta("Type", "Weapon")
+	print("weaponclass ready")
+	
+func _attacking():
+	#get collision data and send damage to game.gd
+	print("attacking with ", selectedWeapon)
+
+func _swapPattern(playerWeapon):
+	#first hide all
+	for i in range (self.get_child_count()):
+		get_node(self.get_child(i).get_name()).hide()
+		print(self.get_child(i).get_name())
+	#then select new pattern
+	selectedWeapon = playerWeapon
+	get_node(selectedWeapon).show()
