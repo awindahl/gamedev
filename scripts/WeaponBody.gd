6319a5cc8f1f65ccc67e5d846920a5404bbd2dd5
awindahl <alewin1994@gmail.com>
2017-12-29 15:33:27 +0100

    fixed unarmed attacking, zoomed out camera a bit

--- scripts/WeaponBody.gd
+++ scripts/WeaponBody.gd
@@ -43,4 +43,8 @@ func _on_Sword_body_enter( body ):
 
 func _on_Unarmed_body_enter( body ):
 	print(body.get_meta("Type"))
-	
+	if body.get_meta("Type") == "Enemy" && body.get_meta("Damaged") == "False":
+		var test = get_world_2d().get_direct_space_state().intersect_point(body.get_pos(),1)
+		get_parent().get_parent().get_parent()._calculate_damage(test[0].collider,damage)
+	else:
+		1+1
